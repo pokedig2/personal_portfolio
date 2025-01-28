c116a85caa446ca53233d8c18446f338f0b833bb
Destin <destin4235@gmail.com>
2025-01-28 12:16:29 -0800

    Css connected to node

---
+++ app.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+/*
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello from Node.js!</h1>');
+});*/
+
+app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+});
